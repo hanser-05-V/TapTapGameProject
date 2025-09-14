4f65aeb4ab331959f4882bd4a434a34c9940a1cd
lay45612 <3185234369@qq.com>
2025-09-14 22:26:41 +0800

    基本文件夹创建

--- Scripts/Manage/GameManage.cs
+++ Scripts/Manage/GameManage.cs
@@ -2,14 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class GameManage : MonoBehaviour
 {
     // Start is called before the first frame update
-       public int a;
     void Start()
     {
-
-
+        
     }
 
     // Update is called once per frame
