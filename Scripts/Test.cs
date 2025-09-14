360f0cae151692d2989d5bc2aaba4c324c881a38
hanser-05-V <202210306108@cdu.edu.cn>
2025-09-14 15:39:26 +0800

    LS 分支的第一次提交

--- Scripts/Test.cs
+++ Scripts/Test.cs
@@ -7,7 +7,7 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("LS 建立的分支");
     }
 
     // Update is called once per frame
