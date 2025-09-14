34df3d52fd9d5869d52b0753721899eb79e6e286
hanser-05-V <202210306108@cdu.edu.cn>
2025-09-14 18:41:20 +0800

    LS分支测试2

--- Scripts/Test.cs
+++ Scripts/Test.cs
@@ -7,9 +7,8 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("LS 建立的分支");
-        Debug.Log("YMJ建立的分支");
-        Debug.Log("Hello Unity!");
+     
+       
     }
 
     // Update is called once per frame
